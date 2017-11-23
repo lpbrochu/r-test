4a942dc8f6fbba6afdfb291887db217fe75e3b9d
Louis-Philippe Brochuy <lpbrochu@gmail.com>
2017-11-23 06:41:52 -0500

    Improvements!

--- deploy.R
+++ deploy.R
@@ -1,15 +1,18 @@
 install.packages("rsconnect")
-install.packages("plumber")
-library("plumber")
+install.packages("devtools")
 library("rsconnect")
+library("devtools")
 
-  r <- getOption("repos")
-  r["CRAN"] <- "http://cran.cnr.berkeley.edu/"
-  options(repos = r)
+
 
 deploy_app <- function(){
-  rsconnect::setAccountInfo(name="lpbrochu", token="4808194A85A61399E2106BB9C4CC1EB7", secret="b9Akzq1/nwA4+u5SwOpa0NWXpH11s5DVLKoS7gpn")
-  rsconnect::deployAPI("r-test-plumber")
+    rsconnect::setAccountInfo(name = "lpbrochu", token = "4808194A85A61399E2106BB9C4CC1EB7", secret = "b9Akzq1/nwA4+u5SwOpa0NWXpH11s5DVLKoS7gpn")
+    rsconnect::deployAPI("r-test-plumber")
 }
 
-deploy_app()
+install_app <- function(repo, version) {
+    install_github(repo = repo, version = version)
+}
+
+install_app("lobrochu/shiny-sample/shinysample", "1.0.0")
+#deploy_app()
